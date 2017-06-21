f76c577a4b7ac83160c8a2437e5df95bce3b5371
Marco Hernandez <marcohern@gmail.com>
2017-06-21 10:49:45 -0500

    added more images, removed files that should not be there

--- src/app/modules/cars/cars.routes.ts
+++ src/app/modules/cars/cars.routes.ts
@@ -7,30 +7,26 @@ import { CarCompareComponent } from './car-compare/car-compare.component';
 
 import { config } from '../../config';
 
-function pushComparePaths(r:Routes): Routes {
-    let prefix = 'car/compare';
-    let acc:string = prefix;
-    for (let i=1; i<config.compare.min; i++) {
-        acc += '/:id'+i;
-    }
-    for (let i=config.compare.min; i<=config.compare.max; i++) {
-        acc += '/:id'+i;
-        console.log("push",acc);
-        r.push({ path: acc, component: CarCompareComponent});
-    }
-    return r;
-}
-
 let routes: Routes = [
-    { path: 'cars'   , component: CarListComponent   },
-    { path: 'car/add', component: CarDetailComponent },
-    { path: 'car/:id', component: CarDetailComponent }
+    { path: 'cars'   , component: CarListComponent   }
 ];
 
 @NgModule({
-    imports: [ RouterModule.forChild(pushComparePaths(routes)) ],
+    imports: [ RouterModule.forChild(CarRoutes.pushComparePaths(routes)) ],
     exports: [ RouterModule ]
 })
 export class CarRoutes {
-
+    static pushComparePaths(r:Routes): Routes {
+        let prefix = 'car/compare';
+        let acc:string = prefix;
+        for (let i=1; i<config.compare.min; i++) {
+            acc += '/:id'+i;
+        }
+        for (let i=config.compare.min; i<=config.compare.max; i++) {
+            acc += '/:id'+i;
+            console.log("push",acc);
+            r.push({ path: acc, component: CarCompareComponent});
+        }
+        return r;
+    }
 }
