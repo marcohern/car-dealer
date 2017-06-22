fce1ad0a18968c049637bf597b0ae10bc5e2ec07
Marco Hernandez <marcohern@gmail.com>
2017-06-22 17:43:15 -0500

    removed useless commented code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -50,17 +50,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.menu.length).toBe(2);
   });
-/*
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));*/
-/*
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
-  }));*/
 });
